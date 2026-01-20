19c4f8537a5f06b627b0cc6f703dc973f30d5d0b
DragonitePlus <1740084968@qq.com>
2026-01-20 17:05:40 +0800

    init

---
+++ src/main/java/edu/hnu/deepaudit/DeepAuditApplication.java
@@ -0,0 +1,13 @@
+package edu.hnu.deepaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepAuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepAuditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hnu/deepaudit/DeepAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hnu.deepaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
